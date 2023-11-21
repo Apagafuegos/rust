10e8959ba81855bcd3565ad553bbbd87ad6d70eb
Apagafuegos <csantos6952@ceuandalucia.es>
2023-11-21 20:46:25 +0100

    Triangulos

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 enum CategoriasFIA {
     Monoplazas,
     Turismos,
@@ -13,9 +14,23 @@ fn main() {
         kind: CategoriasFIA::Monoplazas,
         champion: String::from("Max Verstappen"),
     };
+    println!("{:?}, {:?}", f1.kind, f1.champion);
 
     let wec = Categoria {
         kind: CategoriasFIA::HyperCars,
         champion: String::from("Toyota Gazoo Racing"),
     };
+    println!("{:?},{:?}", wec.kind, wec.champion);
+
+    let gt3: Categoria = Categoria {
+        kind: CategoriasFIA::Turismos,
+        champion: String::from("Carlos Santos"),
+    };
+    println!("{:?},{:?}", gt3.kind, gt3.champion);
+
+    let dakar = Categoria {
+        kind: CategoriasFIA::Rally,
+        champion: String::from("Carlos Sainz"),
+    };
+    println!("{:?},{:?}", dakar.kind, dakar.champion);
 }
--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -9,6 +9,19 @@ impl Rectangle {
     }
 }
 
+#[derive(Debug)]
+struct Triangle {
+    base: u32,
+    height: u32,
+}
+impl Triangle {
+    //Implementa una funcionalidad a un tipo y devuelve un valor, o nada
+    //En este caso, crea una funcion area que llama a los valores de sí mismo para calcular el área del triangulo
+    fn area(&self) -> u32 {
+        (self.base * self.height) / 2
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -19,4 +32,12 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    let tri1 = Triangle {
+        base: 10,
+        height: 33,
+    };
+
+    println!("Tri1 is {:?}", tri1);
+    println!("El área del triangulo es {} cm2", tri1.area());
 }
