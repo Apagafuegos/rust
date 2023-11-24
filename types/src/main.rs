7ee3e612cc64ffb9b8021c5fc582d25ef48fb453
Apagafuegos <csantos6952@ceuandalucia.es>
2023-11-24 20:38:36 +0100

    fin 24/11

--- types/src/main.rs
+++ types/src/main.rs
@@ -9,16 +9,16 @@ fn main() {
 
     println!("El numero más grande es {resultado}");
     //Se pueden hacer structs y enums que almacenen cualquier tipo de dato genérico
-    let _punto_integer = Point{x:5, y:5};
-    let _punto_float = Point{x:5.3, y:3.3};
+    let _punto_integer = Point { x: 5, y: 5 };
+    let _punto_float = Point { x: 5.3, y: 3.3 };
     //Da error si el struct no implementa 2 tipos distintos -> let _punto_mix = Point{x:5.3, y:3};
-    let p = Point{x: 5.3 , y: 33.3};
-    let p2 = Point{x:5, y:3};
-    println!("La coordenada x del punto es {}",p.x());
+    let p = Point { x: 5.3, y: 33.3 };
+    let p2 = Point { x: 5, y: 3 };
+    println!("La coordenada x del punto es {}", p.x());
     println!("La coordenada y del punto es {}", p.y());
     println!("La distancia al origen es de {} ud.", p.distancia_origen());
 
-    println!("La coordenada x del punto es {}",p2.x());
+    println!("La coordenada x del punto es {}", p2.x());
     println!("La coordenada y del punto es {}", p2.y());
     println!("La distancia al origen es de {} ud.", p2.distancia_origen());
 }
@@ -33,15 +33,15 @@ fn mayor(lista: &[i32]) -> &i32 {
     return mayor;
 }
 
-struct Point<T>{
+struct Point<T> {
     x: T,
     y: T,
 } //Defino un tipo llamado punto con dos campos de dos tipos distintos
 
-
-impl Point<f32>{ //Implemento el tipo custom en estas dos funciones
+impl Point<f32> {
+    //Implemento el tipo custom en estas dos funciones
     fn x(&self) -> &f32 {
-        &self.x //"Saco" la componente X del tipo punto 
+        &self.x //"Saco" la componente X del tipo punto
     }
     fn y(&self) -> &f32 {
         &self.y //Igual, pero con la Y
@@ -50,18 +50,17 @@ impl Point<f32>{ //Implemento el tipo custom en estas dos funciones
     fn distancia_origen(&self) -> f32 {
         (self.x.powi(2) + self.y.powi(2)).sqrt()
     }
-    
 }
 
 impl Point<i32> {
     fn x(&self) -> &i32 {
-        &self.x //"Saco" la componente X del tipo punto 
+        &self.x //"Saco" la componente X del tipo punto
     }
     fn y(&self) -> &i32 {
         &self.y //Igual, pero con la Y
     }
 
     fn distancia_origen(&self) -> f32 {
-        (self.x.pow(2)as f32 + self.y.pow(2)as f32).sqrt()
+        (self.x.pow(2) as f32 + self.y.pow(2) as f32).sqrt()
     }
 }
