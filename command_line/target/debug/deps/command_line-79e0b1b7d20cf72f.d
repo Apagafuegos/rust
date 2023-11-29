6ec154c929529e3a8cdadfc89fc6bd96e9704ef1
Apagafuegos <csantos6952@ceuandalucia.es>
2023-11-29 15:41:01 +0100

    :V

---
+++ command_line/target/debug/deps/command_line-79e0b1b7d20cf72f.d
@@ -0,0 +1,5 @@
+D:\rust\command_line\target\debug\deps\command_line-79e0b1b7d20cf72f.exe: src\lib.rs
+
+D:\rust\command_line\target\debug\deps\command_line-79e0b1b7d20cf72f.d: src\lib.rs
+
+src\lib.rs:
