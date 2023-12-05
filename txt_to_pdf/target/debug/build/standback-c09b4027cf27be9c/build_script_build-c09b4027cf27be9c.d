a79e945d567b09801598ba8d53c2b37e5ec708c8
Apagafuegos <csantos6952@ceuandalucia.es>
2023-12-06 00:40:26 +0100

    Cambios del VSCode

--- txt_to_pdf/target/debug/build/standback-c09b4027cf27be9c/build_script_build-c09b4027cf27be9c.d
+++ txt_to_pdf/target/debug/build/standback-c09b4027cf27be9c/build_script_build-c09b4027cf27be9c.d
@@ -1,5 +1,5 @@
-C:\Users\TECH\Downloads\rust\txt_to_pdf\target\debug\build\standback-c09b4027cf27be9c\build_script_build-c09b4027cf27be9c.exe: C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\standback-0.2.17\build.rs
+D:\rust\txt_to_pdf\target\debug\build\standback-c09b4027cf27be9c\build_script_build-c09b4027cf27be9c.exe: C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\standback-0.2.17\build.rs
 
-C:\Users\TECH\Downloads\rust\txt_to_pdf\target\debug\build\standback-c09b4027cf27be9c\build_script_build-c09b4027cf27be9c.d: C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\standback-0.2.17\build.rs
+D:\rust\txt_to_pdf\target\debug\build\standback-c09b4027cf27be9c\build_script_build-c09b4027cf27be9c.d: C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\standback-0.2.17\build.rs
 
-C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\standback-0.2.17\build.rs:
+C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\standback-0.2.17\build.rs:
--- txt_to_pdf/target/debug/deps/const_fn-8cffe620c9c14839.d
+++ txt_to_pdf/target/debug/deps/const_fn-8cffe620c9c14839.d
@@ -1,13 +1,13 @@
-C:\Users\TECH\Downloads\rust\txt_to_pdf\target\debug\deps\const_fn-8cffe620c9c14839.dll: C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\lib.rs C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\error.rs C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\ast.rs C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\iter.rs C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\to_tokens.rs C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\utils.rs C:\Users\TECH\Downloads\rust\txt_to_pdf\target\debug\build\const_fn-bda82e7d98c552a9\out/version
+D:\rust\txt_to_pdf\target\debug\deps\const_fn-8cffe620c9c14839.dll: C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\lib.rs C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\error.rs C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\ast.rs C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\iter.rs C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\to_tokens.rs C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\utils.rs D:\rust\txt_to_pdf\target\debug\build\const_fn-bda82e7d98c552a9\out/version
 
-C:\Users\TECH\Downloads\rust\txt_to_pdf\target\debug\deps\const_fn-8cffe620c9c14839.d: C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\lib.rs C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\error.rs C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\ast.rs C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\iter.rs C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\to_tokens.rs C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\utils.rs C:\Users\TECH\Downloads\rust\txt_to_pdf\target\debug\build\const_fn-bda82e7d98c552a9\out/version
+D:\rust\txt_to_pdf\target\debug\deps\const_fn-8cffe620c9c14839.d: C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\lib.rs C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\error.rs C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\ast.rs C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\iter.rs C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\to_tokens.rs C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\utils.rs D:\rust\txt_to_pdf\target\debug\build\const_fn-bda82e7d98c552a9\out/version
 
-C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\lib.rs:
-C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\error.rs:
-C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\ast.rs:
-C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\iter.rs:
-C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\to_tokens.rs:
-C:\Users\TECH\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\utils.rs:
-C:\Users\TECH\Downloads\rust\txt_to_pdf\target\debug\build\const_fn-bda82e7d98c552a9\out/version:
+C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\lib.rs:
+C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\error.rs:
+C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\ast.rs:
+C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\iter.rs:
+C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\to_tokens.rs:
+C:\Users\carle\.cargo\registry\src\index.crates.io-6f17d22bba15001f\const_fn-0.4.9\src\utils.rs:
+D:\rust\txt_to_pdf\target\debug\build\const_fn-bda82e7d98c552a9\out/version:
 
-# env-dep:OUT_DIR=C:\\Users\\TECH\\Downloads\\rust\\txt_to_pdf\\target\\debug\\build\\const_fn-bda82e7d98c552a9\\out
+# env-dep:OUT_DIR=D:\\rust\\txt_to_pdf\\target\\debug\\build\\const_fn-bda82e7d98c552a9\\out
